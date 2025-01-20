357fdf2928ac5afe7159ceb3597dff72dd1b66b7
Yota Toyama <raviqqe@gmail.com>
2025-01-21 00:06:44 +0900

    Bump version

--- src/ref_mut.rs
+++ src/ref_mut.rs
@@ -2,8 +2,8 @@ use core::marker::PhantomData;
 
 /// A mutable reference.
 ///
-/// This type is purely an annotator for the [IntoAnyFn][crate::IntoAnyFn] trait and
-/// never constructed.
+/// This type is purely an annotator for the [IntoAnyFn][crate::IntoAnyFn] trait
+/// and never constructed.
 pub struct RefMut<T> {
     _data: PhantomData<T>,
 }
