59effeae933b7f65158cec10674fe62f6b6fae85
Yota Toyama <raviqqe@gmail.com>
2025-01-20 20:06:56 +0900

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,92 +1,18 @@
 #![doc = include_str!("../README.md")]
+#![no_std]
 
-use notify::{Event, RecommendedWatcher, RecursiveMode, Watcher};
-use std::{
-    fs::read,
-    mem::take,
-    ops::Deref,
-    path::PathBuf,
-    sync::{OnceLock, RwLock, RwLockReadGuard},
-};
+extern crate alloc;
 
-/// A hot-reloaded module.
-#[derive(Debug)]
-pub struct Module {
-    path: &'static str,
-    current: RwLock<Vec<u8>>,
-    next: RwLock<Option<Vec<u8>>>,
-    watcher: OnceLock<RecommendedWatcher>,
-}
+mod any_fn;
+mod error;
+mod into_any_fn;
 
-/// A read guard of a module.
-#[derive(Debug)]
-pub struct Guard(RwLockReadGuard<'static, Vec<u8>>);
+use alloc::boxed::Box;
+pub use any_fn::*;
+use core::any::Any;
+use core::cell::RefCell;
+pub use error::*;
+pub use into_any_fn::*;
 
-impl Deref for Guard {
-    type Target = [u8];
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl Module {
-    /// Creates a module.
-    pub const fn new(path: &'static str) -> Self {
-        Self {
-            path,
-            current: RwLock::new(Vec::new()),
-            next: RwLock::new(None),
-            watcher: OnceLock::new(),
-        }
-    }
-
-    /// Loads a content.
-    pub fn load(&'static self) -> Guard {
-        self.watcher.get_or_init(|| {
-            let mut lock = self.current.write().unwrap();
-            *lock = read(self.path).unwrap();
-
-            let mut watcher = RecommendedWatcher::new(
-                |result| {
-                    if let Ok(Event { kind, .. }) = result {
-                        if kind.is_modify() {
-                            if let Ok(mut content) = self.next.write() {
-                                *content = Some(read(self.path).unwrap());
-                            }
-                        }
-                    }
-                },
-                Default::default(),
-            )
-            .unwrap();
-            watcher
-                .watch(&PathBuf::from(self.path), RecursiveMode::NonRecursive)
-                .unwrap();
-            watcher
-        });
-
-        // All lock functions used here must be asynchronous.
-        if let Ok(mut content) = self.next.try_write() {
-            if let Some(content) = take(&mut *content) {
-                if let Ok(mut current) = self.current.try_write() {
-                    *current = content;
-                }
-            }
-        }
-
-        Guard(self.current.read().unwrap())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn load_file() {
-        static FOO: Module = Module::new("test/foo.txt");
-
-        assert_eq!(&*FOO.load(), "foo\n".as_bytes());
-    }
-}
+type AnyCell<'a> = &'a RefCell<Box<dyn Any>>;
+type BoxedFunction<'a> = Box<dyn FnMut(&[AnyCell]) -> Result<Box<dyn Any>, DynamicError> + 'a>;
