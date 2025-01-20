92e71b52a2c9ada0653fc3e2c69a91ff21e4a625
Yota Toyama <raviqqe@gmail.com>
2025-01-20 21:44:01 +0900

    Refactor (#14)

--- src/into_any_fn.rs
+++ src/into_any_fn.rs
@@ -9,13 +9,13 @@ pub trait IntoAnyFn<'a, T, S> {
 }
 
 macro_rules! impl_function {
-    ([$($type:ident),*], [$($ref:ident),*]) => {
-        impl<'a, T1: FnMut($($type,)* $(&mut $ref,)*) -> T2 + 'a, T2: Any, $($type: Any + Clone,)* $($ref: Any,)*> IntoAnyFn<'a, ($($type,)* $(RefMut<$ref>,)*), T2> for T1 {
+    ([$($type:ident),*], [$($ref_mut:ident),*]) => {
+        impl<'a, T1: FnMut($($type,)* $(&mut $ref_mut,)*) -> T2 + 'a, T2: Any, $($type: Any + Clone,)* $($ref_mut: Any,)*> IntoAnyFn<'a, ($($type,)* $(RefMut<$ref_mut>,)*), T2> for T1 {
             #[allow(non_snake_case)]
             fn into_any_fn(mut self) -> AnyFn<'a> {
                 #[allow(unused, unused_mut)]
                 AnyFn::new(
-                    (&[$(size_of::<$type>(),)* $(size_of::<$ref>(),)*] as &[usize]).len(),
+                    (&[$(size_of::<$type>(),)* $(size_of::<$ref_mut>(),)*] as &[usize]).len(),
                     Box::new(move |arguments: &[AnyCell]| {
                         let mut iter = 0..;
 
@@ -30,7 +30,7 @@ macro_rules! impl_function {
                             $(
                                 arguments[iter.next().unwrap_or_default()]
                                 .borrow_mut()
-                                .downcast_mut::<$ref>()
+                                .downcast_mut::<$ref_mut>()
                                 .ok_or(AnyFnError::Downcast)?,
                             )*
                         )))
@@ -42,24 +42,24 @@ macro_rules! impl_function {
 }
 
 macro_rules! impl_ref_mut_functions {
-    ([$($type:ident),*], [$first_ref:ident, $($ref:ident),*]) => {
-        impl_function!([$($type),*], [$first_ref, $($ref),*]);
-        impl_ref_mut_functions!([$($type),*], [$($ref),*]);
+    ([$($type:ident),*], [$first_ref_mut:ident, $($ref_mut:ident),*]) => {
+        impl_function!([$($type),*], [$first_ref_mut, $($ref_mut),*]);
+        impl_ref_mut_functions!([$($type),*], [$($ref_mut),*]);
     };
-    ([$($type:ident),*], [$ref:ident]) => {
-        impl_function!([$($type),*], [$ref]);
+    ([$($type:ident),*], [$ref_mut:ident]) => {
+        impl_function!([$($type),*], [$ref_mut]);
         impl_function!([$($type),*], []);
     }
 }
 
 macro_rules! impl_functions {
-    ([$first_type:ident, $($type:ident),*], [$($ref:ident),*]) => {
-        impl_ref_mut_functions!([$first_type, $($type),*], [$($ref),*]);
-        impl_functions!([$($type),*], [$($ref),*]);
+    ([$first_type:ident, $($type:ident),*], [$($ref_mut:ident),*]) => {
+        impl_ref_mut_functions!([$first_type, $($type),*], [$($ref_mut),*]);
+        impl_functions!([$($type),*], [$($ref_mut),*]);
     };
-    ([$type:ident], [$($ref:ident),*]) => {
-        impl_ref_mut_functions!([$type], [$($ref),*]);
-        impl_ref_mut_functions!([], [$($ref),*]);
+    ([$type:ident], [$($ref_mut:ident),*]) => {
+        impl_ref_mut_functions!([$type], [$($ref_mut),*]);
+        impl_ref_mut_functions!([], [$($ref_mut),*]);
     }
 }
 
