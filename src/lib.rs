8aa74b36dec5669841492c07e161e093df34e7cf
Yota Toyama <raviqqe@gmail.com>
2025-01-21 21:08:33 +0900

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -97,7 +97,7 @@ mod tests {
 
         let x = value(0usize);
 
-        <_ as IntoAnyFn<'_, (_, Ref<usize>, _), _>>::into_any_fn(quux)
+        <_ as IntoAnyFn<'_, (_, Ref<usize>, _), _>>::into_any_fn(foo)
             .call(&[&value(40usize), &value(2usize), &x])
             .unwrap();
 
