3fb02dbfb0d4d4a618d073f3ac163ad5d2b861d0
Yota Toyama <raviqqe@gmail.com>
2025-01-24 19:23:48 +0900

    Fix

--- src/value.rs
+++ src/value.rs
@@ -2,3 +2,9 @@ use core::any::Any;
 
 /// A dynianmically-typed value.
 pub struct Value<'a>(&'a RefCell<Box<dyn Any>>);
+
+impl Value {
+    pub fn new<T: Any>(value: T) -> Self {
+        Self(x)
+    }
+}
