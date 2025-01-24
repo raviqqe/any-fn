47239c14f5644977eb157e2c90dd0eb5ba2db7d5
Yota Toyama <raviqqe@gmail.com>
2025-01-24 20:15:08 +0900

    Do not panic on borrow check failures (#34)

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,5 @@
 use core::{
+    cell::{BorrowError, BorrowMutError},
     error::Error,
     fmt::{self, Display, Formatter},
 };
@@ -6,8 +7,24 @@ use core::{
 /// An error.
 #[derive(Debug)]
 pub enum AnyFnError {
+    /// A borrow failure.
+    Borrow(BorrowError),
+    /// A mutable borrow failure.
+    BorrowMut(BorrowMutError),
     /// An object downcast failure.
     Downcast,
+}
+
+impl From<BorrowError> for AnyFnError {
+    fn from(error: BorrowError) -> Self {
+        Self::Borrow(error)
+    }
+}
+
+impl From<BorrowMutError> for AnyFnError {
+    fn from(error: BorrowMutError) -> Self {
+        Self::BorrowMut(error)
+    }
 }
 
 impl Error for AnyFnError {}
@@ -16,6 +33,8 @@ impl Display for AnyFnError {
     fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
         match self {
             Self::Downcast => write!(formatter, "cannot downcast object"),
+            Self::Borrow(error) => write!(formatter, "{error}"),
+            Self::BorrowMut(error) => write!(formatter, "{error}"),
         }
     }
 }
